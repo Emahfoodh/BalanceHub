d3a2654c503e91f7f2d4627e67eb996c0f635d03
Setup boilerplate <setup-boilerplate@example.com>
2025-02-06 06:52:11 +0000

    Initialize boilerplate fullstack-boilerplate-php-laravel-vue

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   env: {
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:8000/api',
   },
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:5000',
     setupNodeEvents(on, config) {},
     supportFile: false,
   },
